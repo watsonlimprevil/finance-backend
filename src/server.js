af315ddbe70518e141872ae6fd87556d2314a5f4
watson limprevil <limprevil35@gmail.com>
2026-07-12 01:09:27 -0400

    fixed a cors issuse inside server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import transactionsRouter from './routes/transactions.js';
 dotenv.config();
 
 const app = express();
-const cors = require('cors')
+
 const cookieParser = require('cookie-parser')
 app.use(cors({
   origin :[
