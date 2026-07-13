3b130862cea7a309530f6b6d2df94ccfa256f801
watson limprevil <limprevil35@gmail.com>
2026-07-13 17:22:26 -0400

    chnaged cors

--- src/server.js
+++ src/server.js
@@ -6,21 +6,30 @@ import transactionsRouter from './routes/transactions.js';
 
 
 dotenv.config();
-app.options('/:path(*)', cors());
+
 
 const app = express();
 app.use(cors({
-  origin :[
-    'http://localhost:5173',
-    'https://finance-frontend-sandy-gamma.vercel.app',
-    "https://finance-frontend-3tbgngsrr-watsonlimprevils-projects.vercel.app",
-    'https://finance-frontend-8dyz57bz1-watsonlimprevils-projects.vercel.app/'
-  ],
-  credentials:true
-}))
+  origin: (origin, callback) => {
+    const allowed = [
+      "http://localhost:5173",
+      "https://finance-frontend-sandy-gamma.vercel.app",
+      "https://finance-frontend-3tbgnsrr-watsonlimprevils-projects.vercel.app",
+      "https://finance-frontend-8dyz57bz1-watsonlimprevils-projects.vercel.app"
+    ];
+
+    if (!origin || allowed.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
-
 // AUTH ROUTES
 app.use('/auth', authRouter);
 
